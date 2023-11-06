1f4155f70579b658fa87b33997cb9e5bef080ff1
arvndblocker"nn <aravindvasang@gmail.com>
2023-11-06 17:45:16 +0530

    testnet deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,8 @@
 require("babel-register");
 require("babel-polyfill");
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { INFURA_API_KEY, MNEMONIC } = process.env;
 
 module.exports = {
   networks: {
@@ -7,6 +10,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(MNEMONIC, INFURA_API_KEY),
+      network_id: "11155111",
+      gas: 4465030,
     },
   },
   contracts_directory: "./src/contracts/",
